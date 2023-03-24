692a5baacb423abfd65a8a5f87e10c21774917af
Booker <3082646292@qq.com>
2023-03-24 09:02:31 +0800

    Myweather

---
+++ mainwindow.cpp
@@ -0,0 +1,60 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QString link="http://restapi.amap.com/v3/weather/weatherInfo?";
+    QString city="110101";
+    QString key="xxxxx";
+    QString stringApi="";
+    stringApi=link;
+    stringApi+="key="+key;
+    stringApi+="&city="+city;
+
+    Manager=new QNetworkAccessManager(this);
+    connect(Manager,&QNetworkAccessManager::finished,this,&MainWindow::recWorker);
+    QUrl url(stringApi);
+    Manager->get(QNetworkRequest(url));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::recWorker(QNetworkReply *reply)
+{
+    // 判断错误码是否为QNetworkReply::NoError（若判断条件成立，则reply对象数据错误）
+    if(reply->error() != QNetworkReply::NoError)
+    {
+        // 弹出警告
+        QMessageBox::warning(nullptr, "警告", "数据返回错误");
+    }
+    else
+    {
+        // 创建array容器接收数据
+        QByteArray data;
+        // 读取所有json串
+        data = reply->readAll();
+        // 解析json串
+        parseJson(data);
+
+    }
+}
+
+void MainWindow::parseJson(QByteArray jsonData)
+{
+    /*将QByteArray类对象转换为QJsonDocument*/
+    QJsonDocument jsonDoc=QJsonDocument::fromJson(jsonData);
+    QJsonObject jsonObject=jsonDoc.object();
+    //qDebug()<<jsonObject.keys()<<endl;
+    QJsonObject lives0=jsonObject.value("lives").toArray().at(0).toObject();
+    for(auto i:lives0.keys())
+    {
+        qDebug()<<i<<":"<<lives0.value(i).toString();
+    }
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QMessageBox>
+#include <QByteArray>
+#include <QJsonValue>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+private:
+   void recWorker(QNetworkReply *);
+   QNetworkAccessManager *Manager;
+   void parseJson(QByteArray jsonData);
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
